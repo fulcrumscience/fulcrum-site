9a96279cc2401bf398b762fadb9c275312738a79
Jonas <jonas@fulcrum.science>
2026-07-14 18:19:23 -0400

    Revert "Remove gathering page"
    This reverts commit 2f7557361a95eb39a23ff02ad160a494284f2c6b.

--- vite.config.js
+++ vite.config.js
@@ -34,6 +34,7 @@ export default defineConfig({
         fellowship: resolve(__dirname, 'fellowship/index.html'),
         fellowshipApply: resolve(__dirname, 'fellowship/apply/index.html'),
         partnerships: resolve(__dirname, 'partnerships/index.html'),
+        gathering: resolve(__dirname, 'gathering/index.html'),
       },
     },
   },
